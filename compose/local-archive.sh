1cab2ed357eb9587f4b527d309833fa99bd4bf8f
JK <jk@chequer.io>
2025-07-29 13:38:43 +0900

    compose/universal: /podman 경로를 /compose/universal 로 이동합니다.
    - 기존 디렉토리 이름을, 실제 의미에 맞게 변경하기 위해, /compose/universal 로 이동합니다.
    - /compose/local-archive.sh 를 추가합니다. 개발 과정에서 테스트할 때, 유용합니다.

---
+++ compose/local-archive.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -o nounset -o errexit -o xtrace
+
+# Local Archive Script
+# This script prepares the testing environment by creating a package archive and copying setup script to home directory.
+#
+# This script creates ~/package.tar.gz file for setup.v2.sh in local development environment.
+# Purpose:
+# - To test setup.v2.sh
+# - To test configuration files in compose/universal/
+
+SCRIPT_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+
+[[ -f ~/package.tar.gz ]] && rm -f ~/package.tar.gz
+
+pushd "$SCRIPT_DIR/universal"
+git pull --rebase
+tar zcvf ~/package.tar.gz .
+
+
+[[ -f ~/setup.v2.sh ]] && rm -f ~/setup.v2.sh
+cp ../aws-ami/scripts/setup.v2.sh ~/setup.v2.sh
