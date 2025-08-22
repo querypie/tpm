2c0f8d297770384ac8d3dfbc6ff8af30a3e87e9e
JK <jk@querypie.com>
2025-08-22 20:03:09 +0900

    AWS: ec2 도구와 ami-ls.sh 를 개선합니다. (#91)
    ## 변경사항
    
    ### `/aws/ec2` 스크립트를 개선합니다. `ec2 terminate` 명령을 수행할 수 있습니다.
      - Terminate 대상을 보여주고, yes 입력을 받아 삭제합니다.
      - 목록 보여주기 화면을 개선합니다.
    ```
    % ./ec2
    EC2 Instance Management Script v25.08.2
    
    Usage:
      ./ec2 start     - Start all instances owned by jk
      ./ec2 stop      - Stop all instances owned by jk
      ./ec2 terminate - Terminate instances owned by jk (per-instance confirmation)
      ./ec2 status    - List instances owned by jk
      ./ec2 help      - Show this help message
    
    Note: This script automatically finds and manages all EC2 instances
          tagged with Owner=jk (case-insensitive matching)
    ```
    
    ### `ami-ls.sh` 스크립트를 개선합니다.
      - owner, name 을 입력받아, 해당하는 AMI 목록을 조회할 수 있습니다.
    ```
    % ./ami-ls.sh --help
    Usage: ./ami-ls.sh [self|marketplace|redhat|rocky|canonical] [name]
    
    EXAMPLES:
      ./ami-ls.sh self querypie
      ./ami-ls.sh rocky Rocky-8-
    %
    ```

--- aws/ami/ami-ls.sh
+++ aws/ami/ami-ls.sh
@@ -1,9 +1,50 @@
 #!/usr/bin/env bash
 
-set -o xtrace
+function list_ami_images() {
+  local owners=$1 name=${2:-}
 
-aws ec2 describe-images \
-  --owners self \
-  --query 'Images[*].{Name:Name,ImageId:ImageId,State:State,CreationDate:CreationDate,Description:Description,Architecture:Architecture,VirtualizationType:VirtualizationType}' \
-  --output table
+  printf "Name\tImageId\tState\tCreationDate\tDescription\tArch\tVType\tDevice\n"
+  # Run the AWS CLI command and capture output into tmp_file
+  aws ec2 describe-images \
+    --owners "${owners}" \
+    --filters "Name=name,Values=${name}*" \
+    --query 'Images[][Name, ImageId, State, CreationDate, Description, Architecture, VirtualizationType, BlockDeviceMappings[0].DeviceName]' \
+    --output text |
+    sed -e 's/\bNone\b/-/g' |
+    column -t -s $'\t'
+}
 
+function main() {
+  local owner=${1:-self} name=${2:-} owners
+  case "$owner" in
+  self)
+    owners="self"
+    ;;
+  marketplace)
+    owners="679593333241" # AWS Marketplace
+    ;;
+  redhat)
+    owners="309956199498" # Red Hat Enterprise Linux
+    ;;
+  rocky)
+    owners="792107900819" # Rocky Linux
+    ;;
+  canonical)
+    owners="099720109477" # Canonical
+    ;;
+  *)
+    cat <<EOF
+Usage: $0 [self|marketplace|redhat|rocky|canonical] [name]
+
+EXAMPLES:
+  $0 self querypie
+  $0 rocky Rocky-8-
+EOF
+    exit 1
+    ;;
+  esac
+
+  list_ami_images "${owners}" "${name}"
+}
+
+main "$@"
