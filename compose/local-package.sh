865fd7915fa69bc9b58c836669d696cd7b940950
JK <jk@chequer.io>
2025-08-26 21:26:09 +0900

    setup.v2.sh: 폐쇄망 환경 설치 때, ./offline/ 디렉토리 내 파일을 사용
    - 폐쇄망 환경 설치를 위해, `./offline/` 디렉토리를 참조합니다.
      - `./offline/` 아래에서 설치에 필요한 파일을 찾는 경우, 그 파일을 설치에 사용합니다.
      - `package.tar.gz` 을 참조합니다.
      - `docker-compose-${kernel}-${hardware}` 를 참조합니다.
    - `rhel-8.pkr.hcl` 리팩토링
      - No final cleanup in rhel-8.pkr.hcl : 불필요한 파일 삭제를 생략합니다.
    - `compose/offline-package.sh` 를 구현합니다.
      - `compose/offline`에 설치용 파일을 저장하는 스크립트입니다.
    - `compose/local-package.sh` 는 `~/offline/package.tar.gz` 파일을 생성하도록 변경합니다.

--- compose/local-package.sh
+++ compose/local-package.sh
@@ -2,21 +2,24 @@
 set -o nounset -o errexit -o xtrace
 
 # Local Archive Script
-# This script prepares the testing environment by creating a package archive and copying setup script to home directory.
+# This script prepares the testing environment by creating a package archive and copying the setup script to a home directory.
 #
-# This script creates ~/package.tar.gz file for setup.v2.sh in local development environment.
+# This script creates the ~/offline / package.tar.gz file for setup.v2.sh in a local development environment.
 # Purpose:
 # - To test setup.v2.sh
 # - To test configuration files in compose/universal/
 
 SCRIPT_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
 
-[[ -f ~/package.tar.gz ]] && rm -f ~/package.tar.gz
+if [[ -d ~/offline ]]; then
+  [[ -f ~/offline/package.tar.gz ]] && rm -f ~/offline/package.tar.gz
+else
+  mkdir -p ~/offline
+fi
 
 pushd "$SCRIPT_DIR/universal"
 git pull --rebase
-tar zcvf ~/package.tar.gz .
-
+tar zcvf ~/offline/package.tar.gz .
 
 [[ -f ~/setup.v2.sh ]] && rm -f ~/setup.v2.sh
 [[ -f ../../aws/scripts/setup.v2.sh ]] &&
